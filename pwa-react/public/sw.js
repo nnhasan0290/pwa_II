ba447c266d637f5d36f5b1edd4bb523c694cd860
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-02-05 00:39:38 +0000

    bare minimum done

--- pwa-react/public/sw.js
+++ pwa-react/public/sw.js
@@ -1,1 +1,22 @@
-console.log("sw js is connected successfully")
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/bundle.js",
+        "/index.html",
+        "/",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => {
+            if(res){
+                return res
+            }
+        })
+    )
+})
