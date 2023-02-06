ddfb24ac927bf4d14fda3fe7bdfc557131694d43
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-02-06 00:57:40 +0000

    show data by checking internet connection

--- pwa-react/public/sw.js
+++ pwa-react/public/sw.js
@@ -2,21 +2,19 @@ let cacheData = "appV1";
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        "/static/js/bundle.js",
-        "/index.html",
-        "/",
-      ]);
+      cache.addAll(["/static/js/bundle.js", "/index.html", "/"]);
     })
   );
 });
 
 this.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
     event.respondWith(
-        caches.match(event.request).then((res) => {
-            if(res){
-                return res
-            }
-        })
-    )
-})
+      caches.match(event.request).then((res) => {
+        if (res) {
+          return res;
+        }
+      })
+    );
+  }
+});
--- pwa-react/src/App.js
+++ pwa-react/src/App.js
@@ -1,80 +1,31 @@
+import React from "react";
+
 function App() {
+  const [post, setPosts] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
       <main>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
-        <p>this is paragraph</p>
+        {post.length &&
+          post.map((each) => (
+            <div
+              style={{
+                width: "80%",
+                marginRight: "auto",
+                marginLeft: "auto",
+                border: "1px solid gray",
+                padding: "20px"
+              }}
+              key={each.id}
+            >
+              <h2>{each.title}</h2>
+              <p>{each.body}</p>
+            </div>
+          ))}
       </main>
     </div>
   );
